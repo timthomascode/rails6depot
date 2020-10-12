878dae5d0854df26ac15d3c6e52806fbf56054fd
gratefulcheddar <timthomascode@icloud.com>
2020-10-11 22:07:25 -0500

    Task F

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -5,9 +5,15 @@ class Cart < ApplicationRecord
     current_item = line_items.find_by(product_id: product.id)
     if current_item
       current_item.quantity += 1
+      # current_item.price += product.price 
     else
       current_item = line_items.build(product_id: product.id)
     end
     current_item
   end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
+
 end
